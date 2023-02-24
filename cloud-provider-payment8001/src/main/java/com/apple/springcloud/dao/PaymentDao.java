639386402bb9e16a7d28fd2443e0552c5ac2dccf
wp <2838839039@qq.com>
2023-02-24 08:17:12 +0800

    提交所有文件

---
+++ cloud-provider-payment8001/src/main/java/com/apple/springcloud/dao/PaymentDao.java
@@ -0,0 +1,13 @@
+package com.apple.springcloud.dao;
+
+import com.apple.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
---
+++ cloud-provider-payment8001/src/main/java/com/apple/springcloud/service/PaymentService.java
@@ -0,0 +1,10 @@
+package com.apple.springcloud.service;
+
+import com.apple.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+    public int create(Payment payment);
+
+    public Payment getPaymentById(Long id);
+}
